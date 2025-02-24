517a3ec77b3a64dbd0c95ac0b6106082d64f00a3
emilyytsai <emilytsai@csu.fullerton.edu>
2025-02-23 20:22:49 -0800

    Free roam cam no longer uses GetAxis; debugged

--- Assets/scripts/Player controler/CameraController.cs
+++ Assets/scripts/Player controler/CameraController.cs
@@ -7,6 +7,7 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField]
     private Camera cam;
+    private InputSystem_Actions inputActions; //reference to input sys
 
     [SerializeField]
     private float moveSpeed = 10f;
@@ -17,16 +18,14 @@ public class CameraController : MonoBehaviour
     private float mapMinX, mapMaxX, mapMinY, mapMaxY;
 
     private PlayerInput playerInput; //using unity input sys
-    private Vector2 moveInput;
-    private float zoomInput;
+    private Vector2 moveInput; //WASD
+    private float zoomInput; //mouse scroll wheel
 
     private void Awake()
     {
-        playerInput = new PlayerInput();
-        playerInput.Camera.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
-        playerInput.Camera.Move.canceled += ctx => moveInput = Vector2.zero;
-        playerInput.Camera.Zoom.performed += ctx => zoomInput = ctx.ReadValue<float>();
-        playerInput.Camera.Zoom.canceled += ctx => zoomInput = 0f;
+        //playerInput = GetComponent<PlayerInput>(); //GetComponent gets the PlayerInput attached to game object (the backgroudn)
+        inputActions = new InputSystem_Actions(); //initialize InputSystem_Actions(input actions editor)
+        inputActions.Enable();  //enable the input actions
 
         mapMinX = mapRenderer.transform.position.x - mapRenderer.bounds.size.x / 2f;
         mapMaxX = mapRenderer.transform.position.x + mapRenderer.bounds.size.x / 2f;
@@ -35,30 +34,54 @@ public class CameraController : MonoBehaviour
         mapMaxY = mapRenderer.transform.position.y + mapRenderer.bounds.size.y / 2f;
     }
 
-    private void OnEnable() => playerInput.Enable();
-    private void OnDisable() => playerInput.Disable();
-
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        MoveCamera();
-        Zoom();
+        inputActions.Camera.Move.performed += MoveCamera;  //move binding
+        //inputActions.Camera.Zoom.performed += Zoom;  //zoom binding
     }
 
-    private void MoveCamera()
+    private void OnDisable()
     {
-        Vector3 moveVector = new Vector3(moveInput.x, moveInput.y, 0) * moveSpeed * Time.deltaTime;
-        cam.transform.position = ClampCamera(cam.transform.position + moveVector);
+        inputActions.Disable();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //MoveCamera();
+        //continuous movement input; holding down on WASD
+
+        Vector2 move = inputActions.Camera.Move.ReadValue<Vector2>();
+
+        Vector3 newPosition = cam.transform.position + new Vector3(move.x, move.y, 0) * moveSpeed * Time.deltaTime;
+        cam.transform.position = ClampCamera(newPosition);
+
+        if (inputActions.Camera.Zoom != null)
+        {
+            zoomInput = Mouse.current.scroll.ReadValue().y; // read zoom input
+            Zoom(); // call Zoom without parameters
+        }
+    }
+
+    private void MoveCamera(InputAction.CallbackContext ctx)
+    {
+        Vector2 move = ctx.ReadValue<Vector2>(); //read move input; WASD
+        Vector3 newPosition = cam.transform.position + new Vector3(move.x, move.y, 0) * moveSpeed * Time.deltaTime;
+        cam.transform.position = ClampCamera(newPosition);
     }
 
     private void Zoom()
     {
-        if (scroll != 0)
-        {
-            float newSize = cam.orthographicSize - scroll * zoomStep;
-            cam.orthographicSize = Mathf.Clamp(newSize, minCamSize, maxCamSize);
-            cam.transform.position = ClampCamera(cam.transform.position);
-        }
+        //float zoomInput = ctx.ReadValue<float>(); //read zoom input; scroll wheel
+        //float newSize = cam.orthographicSize - zoomInput * zoomStep;
+        
+        //scroll up = zoom in; decrease orthographic size
+        //scroll down = zoom out; increase orthographic size
+        float zoomChange = zoomInput * zoomStep;
+        float newSize = cam.orthographicSize - zoomChange;
+        
+        cam.orthographicSize = Mathf.Clamp(newSize, minCamSize, maxCamSize);
+        cam.transform.position = ClampCamera(cam.transform.position);
     }
 
     private Vector3 ClampCamera(Vector3 targetPosition)
