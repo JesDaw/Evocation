06b39bb94596bb16fa26709bc617932a525a6f2c
JesDaw <jd281003@gmail.com>
2025-02-23 20:24:18 -0800

    Started script for game lose by player death
    Started script for game lose by player death

--- Assets/scripts/Game systems/PlayerLivesManager.cs
+++ Assets/scripts/Game systems/PlayerLivesManager.cs
@@ -1,22 +1,31 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 
 //script need a corsponding player deth tracker
 public class PlayerLivesManager : MonoBehaviour
 {
    [SerializeField]  IntVeriable LifeCount;
-    void Start()
-    {
-        
-    }
+    public UnityEvent PlayerDeath;
+    public UnityEvent PlayerSpawn;
+    [SerializeField] IntVeriable MaxLives;
+    bool canSpawnMore;
 
-    // Update is called once per frame
-    void GaimLife()
+    public void GainLife()
     {
         LifeCount._Value++;
+        if (LifeCount._Value == MaxLives._Value)
+        {
+            canSpawnMore =false;
+        }
     }
-    void LooseLife()
+    public void LooseLife()
     {
         LifeCount._Value--;
+        canSpawnMore = true;
+        if (LifeCount._Value == 0)
+        {
+            //invoke game over event
+        }
     }
 }
