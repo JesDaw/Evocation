73a9e70b53bdbd58033d737363b7fd9dfcefc715
Homan Qiu <thehuiq2006@gmail.com>
2025-02-23 20:01:00 -0800

    QuickUpdate

---
+++ Assets/Scripts/CpuUnits/CpuController.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Events;
+
+public class CpuController : MonoBehaviour
+{
+    public UnityEvent SpawnNormal;
+
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/CpuUnits/ScriptableStats.cs
+++ Assets/Scripts/CpuUnits/ScriptableStats.cs
@@ -8,11 +8,14 @@ public class ScriptableStats : ScriptableObject
     public Sprite _Sprite;
     public string _Clan;
     public int _Health;
+    //for healing just change attack to a negaitve number
     public int _Attack;
     public float _AttackSpeed;
     public float _Speed;
     public float _StopDistance;
     public float _KnockBackVelocity;
     public float _KnockBackHealth;
+    public enum AttackType {Projectile, AOE, Direct}
+    public AttackType _AttackType = AttackType.Direct;
 }
     
