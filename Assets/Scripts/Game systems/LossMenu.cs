679daf7a4008a4e23f51d3dec7f320604ff0ae01
Homan Qiu <thehuiq2006@gmail.com>
2025-02-27 22:19:15 -0800

    CherryPicked Update
    This reverts commit e6d1035fc1f6f95e70e08692d4d35323afab285b.

---
+++ Assets/Scripts/Game systems/LossMenu.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class LossMenu : MonoBehaviour
+{
+    void Update()
+    {
+      //  if (Input.GetKeyDown(KeyCode.L))
+      //  {
+       //     defeatMenuUI.SetActive(true);
+       //     GameIsOver = true;
+       // }
+    }
+}
