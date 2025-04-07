f73c4450101b2bfdade57137b7a1747d140951b3
AdamLenzini <adamclenzini@gmail.com>
2025-04-06 20:51:54 -0700

    Transitions
    Transition animations created, but need to be implemented with main menu

--- Assets/scripts/Game systems/Menus/MainMenu.cs
+++ Assets/scripts/Game systems/Menus/MainMenu.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
 public class MainMenu : MonoBehaviour
 {
+
    //for mouse click sound effect
    private AudioManager audio_manager;
 
@@ -14,11 +14,11 @@ public class MainMenu : MonoBehaviour
 
    public void PlayGame()
    {
-       //SceneManager.LoadScene("Level 1");
-      //SceneManager.LoadScene("Week 3 stuff");
+        //SceneManager.LoadScene("Level 1");
+        //SceneManager.LoadScene("Week 3 stuff");
 
-      //for testing if audio changes on a different scene
-      SceneManager.LoadScene("Week 6 stuff");
+        //for testing if audio changes on a different scene
+        SceneManager.LoadScene("Week 6 stuff");
    }
 
    public void click_sound()
