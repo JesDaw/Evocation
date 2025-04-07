dd34f0091dc2e1c3b5f5da4b2cfcf9d0bca46a14
emilyytsai <emilytsai@csu.fullerton.edu>
2025-04-06 20:44:57 -0700

    Added resolution dropdown menu in settings

--- Assets/Scripts/Game systems/Menus/SettingsMenu.cs
+++ Assets/Scripts/Game systems/Menus/SettingsMenu.cs
@@ -1,19 +1,50 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Audio;
+using System.Collections.Generic;
 
 public class SettingsMenu : MonoBehaviour
 {
     public Slider volume_slider;
+    public TMPro.TMP_Dropdown resolution_dropdown;
     public AudioMixer audio_mixer;
+
+    Resolution[] resolutions;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //using player pref to save the volume setting acorss diff scenes
+        //volume//////
         float saved_volume = PlayerPrefs.GetFloat("Volume", 1f);
         set_volume(saved_volume);
         volume_slider.value = saved_volume;
+
+        //quality/////
+        int saved_quality = PlayerPrefs.GetInt("GraphicsQuality", QualitySettings.GetQualityLevel());
+        set_quality(saved_quality);
+
+        //resolutions////////
+        resolutions = Screen.resolutions;
+        resolution_dropdown.ClearOptions();
+        //turn array of resolutions into formatetd strings
+        List<string> options = new List<string>();
+
+        int current_res = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                current_res = i;
+            }
+        }
+        resolution_dropdown.AddOptions(options);
+        resolution_dropdown.value = current_res;
+        resolution_dropdown.RefreshShownValue();
     }
 
     public void set_volume(float volume)
@@ -33,5 +64,11 @@ public class SettingsMenu : MonoBehaviour
     public void set_fullscreen (bool is_fullscreen)
     {
         Screen.fullScreen = is_fullscreen;
+    }
+
+    public void set_resolution (int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }
