da5b32e6216170c321be4acb97791442ae233e9c
JesDaw <jd281003@gmail.com>
2025-06-08 19:09:07 -0700

    Updated background and make characters walk on bridge

---
+++ Assets/Code/scripts/Levels/Gameplay/Systems/LevelMid/Time/Timer.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.Events;
+using TMPro;
+
+
+// Countdown timer functioality 
+// takes a number of seconds and a UI object from unity interface
+// convrts seconds to minuts and seconds and counts down to 0 from there 
+// desplays time in provided UI object
+public class Timer : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] UnityEvent _TimeHitZero;
+    public FloatVariable remainingTimeSeconds;
+
+    bool _game_is_active = true;
+
+    void Update()
+    {
+        if (_game_is_active)
+        {
+            Countdown();
+        }
+        
+        DesplayTime();
+    }
+
+    //counts down that stops at 0
+    void Countdown()
+    {
+        if (remainingTimeSeconds._Value > 0)
+        {
+            remainingTimeSeconds._Value -= Time.deltaTime; 
+        }
+        else 
+        {
+            remainingTimeSeconds._Value = 0;
+            _TimeHitZero.Invoke();
+            DeactivateTimer();    
+        }
+    }
+
+    public void DeactivateTimer(){ _game_is_active = false; }
+
+    public void ResetTimer(){ remainingTimeSeconds.Reset();}
+
+    // conversion from seconds to minuts and seconds and desplays it in UI
+    void DesplayTime()
+    {
+        int minutes = Mathf.FloorToInt(remainingTimeSeconds._Value / 60);
+        int seconds = Mathf.FloorToInt(remainingTimeSeconds._Value % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
