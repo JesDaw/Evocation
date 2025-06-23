JesDaw/Evocation

commit e2ad6289212b6f081f07695f41907c05bbdd5d89

Reorganized the Tests directory and added unit tests for PlayerHealthbar

Assets/Code/scripts/Levels/Gameplay/Objects/Characters/AllCharacterQualities/Stats/Stats.cs
@@ -8,6 +8,7 @@ public class Stats : MonoBehaviour
 {
     public List<string> _CpuPriority;
     public string _Clan;
+    public int _MaxHealth = 1;
     public int _Health = 1;
     public int _AttackDamage;
     public int _AttackStartup;

Assets/Code/AssemblyInfo.cs
@@ -1,0 +1,5 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+[assembly: InternalsVisibleTo("Tests.dll")]
+[assembly: InternalsVisibleTo("Tests")]

Assets/Code/AssemblyInfo.cs.meta
@@ -1,0 +1,2 @@
+fileFormatVersion: 2
+guid: 269af5fa4a20f1f43aa1d7a35b30b62f

Assets/Code/CodeAssembly.asmdef
@@ -1,0 +1,19 @@
+{
+    "name": "CodeAssembly",
+    "rootNamespace": "",
+    "references": [
+        "GUID:57a6d82589ae03c4cabdc058f719f231",
+        "GUID:75469ad4d38634e559750d17036d5f7c",
+        "GUID:6055be8ebefd69e48b49212b09b47b2f",
+        "GUID:4307f53044263cf4b835bd812fc161a4"
+    ],
+    "includePlatforms": [],
+    "excludePlatforms": [],
+    "allowUnsafeCode": false,
+    "overrideReferences": false,
+    "precompiledReferences": [],
+    "autoReferenced": true,
+    "defineConstraints": [],
+    "versionDefines": [],
+    "noEngineReferences": false
+}

Assets/Code/CodeAssembly.asmdef.meta
@@ -1,0 +1,7 @@
+fileFormatVersion: 2
+guid: 2016f37038df12644892074f41add7a9
+AssemblyDefinitionImporter:
+  externalObjects: {}
+  userData: 
+  assetBundleName: 
+  assetBundleVariant: 

Assets/Code/scripts/Levels/Gameplay/UI/PlayerHealthbar.cs
@@ -3,10 +3,25 @@ using UnityEngine.UI;
 
 public class PlayerHealthbar : MonoBehaviour
 {
-    [SerializeField] ActivePlayer ActivePlayer;
-    [SerializeField] Slider _Slider;
+    [SerializeField] internal ActivePlayer ActivePlayer;
+    [SerializeField] internal Slider _Slider;
 
-    void Start()
+    internal delegate void UpdateHealth(int value);
+    internal delegate void UpdateMaxHealth(int value);
+
+    // Function called to update the Health indicator
+    internal UpdateHealth updateHealthIndicator;
+
+    // Function called to update the Max Health indicator
+    internal UpdateMaxHealth updateMaxHealthIndicator;
+
+    internal PlayerHealthbar()
+    {
+        updateHealthIndicator = (x) => _Slider.value = x;
+        updateMaxHealthIndicator = (x) => _Slider.maxValue = x;
+    }
+
+    internal void Start()
     {
         Debug.Assert(ActivePlayer != null, "This should've already been set via the Editor!");
 
@@ -23,7 +38,7 @@ public class PlayerHealthbar : MonoBehaviour
     /// </summary>
     /// <param name="player">A player GameObject, 'null' defaults to the Active Player</param>
     /// <param name="result">Current stats</param>
-    void getPlayerStats(GameObject player, out Stats result)
+    internal void getPlayerStats(GameObject player, out Stats result)
     {
         result = null;
 
@@ -32,13 +47,16 @@ public class PlayerHealthbar : MonoBehaviour
             player = ActivePlayer.CurrentPlayer;
         }
 
-        if ((player != null) && player.TryGetComponent<Stats>(out Stats stats))
+        if (player != null)
         {
-            result = stats;
-        }
-        else
-        {
-            Debug.LogWarning($"Stats component not found on player {player}!");
+            if (player.TryGetComponent<Stats>(out Stats stats))
+            {
+                result = stats;
+            }
+            else
+            {
+                Debug.LogWarning($"Stats component not found on player {player}!");
+            }
         }
     }
 
@@ -46,7 +64,7 @@ public class PlayerHealthbar : MonoBehaviour
     /// Get the active player stats
     /// </summary>
     /// <param name="result">Player Stats</param>
-    void getPlayerStats(out Stats result)
+    internal void getPlayerStats(out Stats result)
     {
         getPlayerStats(null, out Stats stats);
         result = stats;
@@ -55,20 +73,18 @@ public class PlayerHealthbar : MonoBehaviour
     /// <summary>
     /// Set the HealthBar according to the active player stats
     /// </summary>
-    public void UpdateHealth()
+    public void updateHealthStats()
     {
         getPlayerStats(out Stats stats);
         if (stats != null)
         {
-            // FIXME:  
-            //    We need a stats._MaxHealth (or something equivalent) to
-            //    set the slider with.
-            //
-            //    The Healthbar won't work properly without it when switching
-            //    between players because the bar will appear full even when
-            //    the newly active player is injured.
-            _Slider.maxValue = stats._Health;
-            _Slider.value = stats._Health;
+            updateMaxHealthIndicator(stats._MaxHealth);
+            updateHealthIndicator(stats._Health);
+        }
+        else
+        {
+            updateMaxHealthIndicator(0);
+            updateHealthIndicator(0);
         }
     }
 
@@ -76,7 +92,7 @@ public class PlayerHealthbar : MonoBehaviour
     /// Called when the active player is about to be switched
     /// </summary>
     /// <param name="player">The player about to be deactivated</param>
-    void OnPlayerDeactivating(GameObject player)
+    internal void OnPlayerDeactivating(GameObject player)
     {
         Debug.Assert(player == ActivePlayer.CurrentPlayer);
         getPlayerStats(player, out Stats stats);
@@ -91,7 +107,7 @@ public class PlayerHealthbar : MonoBehaviour
     /// Called when a player is 'activated'
     /// </summary>
     /// <param name="player">New, active player</param>
-    void OnPlayerActivating(GameObject player)
+    internal void OnPlayerActivating(GameObject player)
     {
         Debug.Assert(player == ActivePlayer.CurrentPlayer);
         getPlayerStats(player, out Stats stats);
@@ -101,19 +117,19 @@ public class PlayerHealthbar : MonoBehaviour
             stats.OnDeath += OnActivePlayerDeath;
         }
 
-        UpdateHealth();
+        updateHealthStats();
     }
 
     /// <summary>
     /// Called when the currently active player takes damage so
     /// that the health bar can be updated
     /// </summary>
-    void OnActivePlayerDamage()
+    internal void OnActivePlayerDamage()
     {
         getPlayerStats(out Stats stats);
         if (stats != null)
         {
-            _Slider.value = stats._Health;
+            updateHealthIndicator(stats._Health);
         }
     }
 
@@ -121,8 +137,8 @@ public class PlayerHealthbar : MonoBehaviour
     /// Called when the currently active player dies so the
     /// health bar can be updated.
     /// </summary>
-    void OnActivePlayerDeath()
+    internal void OnActivePlayerDeath()
     {
-        _Slider.value = 0;
+        updateHealthIndicator(0);
     }
 }

Assets/Tests/Code.meta
@@ -1,0 +1,8 @@
+fileFormatVersion: 2
+guid: 0473f510dd6772a4691c4c5e4da35e0b
+folderAsset: yes
+DefaultImporter:
+  externalObjects: {}
+  userData: 
+  assetBundleName: 
+  assetBundleVariant: 

Assets/Tests/Code/Scripts.meta
@@ -1,0 +1,8 @@
+fileFormatVersion: 2
+guid: 578a2d2288588fb4cb95ef3c3e565a48
+folderAsset: yes
+DefaultImporter:
+  externalObjects: {}
+  userData: 
+  assetBundleName: 
+  assetBundleVariant: 

commit 989d3763f4d665061a0f4d3419699ccc66d0a6b4

Fixed Stats.cs related issues mistakenly introduced in e2ad6289212b6f081f07695f41907c05bbdd5d89

Assets/Code/scripts/Levels/Gameplay/Objects/Characters/AllCharacterQualities/Stats/Stats.cs
@@ -10,7 +10,8 @@ public class Stats : MonoBehaviour
     public string _Clan;
 
     //the above can be made into an enum, but i'll hold off on it until we get all the different clans
-    public int _Health = 1;
+    public int _MaxHealth = 1;
+    public int _CurrentHealth = 1;
     public int _AttackDamage;
     public int _AttackStartup;
     public int _AttackActiveDuration;
@@ -28,8 +29,8 @@ public class Stats : MonoBehaviour
     public List<Vector2> _StatusTicks;
     public int _StatusMax;
     public int _StatusHealth;
-    [SerializeField] UltEvents.UltEvent OnDeath, OnDamage;
-    [SerializeField] UltEvents.UltEvent<bool> OnWitFlagDeath, OnWitFlagDamage;
+    [SerializeField] internal UltEvents.UltEvent OnDeath, OnDamage;
+    [SerializeField] internal UltEvents.UltEvent<bool> OnWitFlagDeath, OnWitFlagDamage;
     //the reason this is public is because it will be applied from the
     //scriptable objects
 
@@ -47,6 +48,7 @@ public class Stats : MonoBehaviour
     public void Start()
     {
         StartCoroutine(StatusEffectLoop());
+        _CurrentHealth = _MaxHealth;
     }
 
     IEnumerator StatusEffectLoop()
@@ -97,13 +99,13 @@ public class Stats : MonoBehaviour
     {
         if (_Invincible) return;
 
-        _Health -= _Damage;
+        _CurrentHealth -= _Damage;
         _KnockBackHealth -= _Damage;
 
         if (_AttackedBy != null) OnWitFlagDamage.Invoke(_AttackedBy.IsEnemy);
         OnDamage.Invoke();
 
-        if (_Health <= 0)
+        if (_CurrentHealth <= 0)
         {
             Died();    
         }
@@ -125,7 +127,7 @@ public class Stats : MonoBehaviour
     }
     public void SetHealth(int _Amount)
     {
-        _Health = _Amount;
+        _CurrentHealth = _Amount;
     }
 
     public void AddStatusEffect(StatusEffect _effect)

Assets/Code/scripts/Levels/Gameplay/Objects/Structures&Hazards/BuildingStatsManager.cs
@@ -17,14 +17,14 @@ public class BuildingStatsManager : MonoBehaviour
 
     public void SetMax()
     {
-        _Stats._Health = ScrStats._Health;
+        _Stats._MaxHealth = ScrStats._MaxHealth;
     }
 
     public void SwapBuilding(ScriptableStats _ScrStats)
     {
         _Stats._Clan = _ScrStats._Clan;
         gameObject.tag = _Stats._Clan;
-        _Stats._Health = _ScrStats._Health;
+        _Stats._CurrentHealth = _ScrStats._CurrentHealth;
         _Stats._AttackDamage = _ScrStats._AttackDamage;
         _Stats._AttackEndlag = _ScrStats._AttackEndlag;
         _Stats._MoveSpeed = _ScrStats._MoveSpeed;