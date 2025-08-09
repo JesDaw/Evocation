2d5646c334155dceb70b4b1b0e73786bbbd96f08
JesDaw <jd281003@gmail.com>
2025-08-08 18:45:01 -0700

    Finished core or dynamic music system

---
+++ Assets/Code/scripts/General/Audio/MusicSystem/MusicManager.cs
@@ -0,0 +1,207 @@
+using System.Collections.Generic;
+using UnityEngine;
+using static IMusicTransition;
+
+public class MusicManager : MonoBehaviour
+{
+    AudioSource _AnchoringTrack;
+    List<AudioSource> _currentTracks = new List<AudioSource>();
+    [SerializeField] public List<AudioSource> Tracks = new List<AudioSource>();
+    
+    private Dictionary<AudioSource, bool> _trackFadingStates = new Dictionary<AudioSource, bool>();
+    List<AudioEffectEditor> _currentEffects = new List<AudioEffectEditor>();
+
+    void Start()
+    {
+        foreach (AudioSource track in Tracks)
+        {
+            _trackFadingStates[track] = false;
+        }
+
+        Debug.Log("Track list:");
+        foreach (AudioSource track in Tracks)
+        {
+            Debug.Log(track.clip.name);
+        }
+    }
+
+    void Update()
+    {
+        _AnchoringTrack = _currentTracks.Find(track => track.isPlaying);
+    }
+
+    void CurrentTrackCleanUp()
+    {
+        foreach (AudioEffectEditor audioEffectEditoreffect in _currentEffects)
+        {
+            if (!audioEffectEditoreffect.Track.isPlaying)
+            {
+                audioEffectEditoreffect.ResetEffects();
+            }
+        }
+        _currentTracks.RemoveAll(track => !track.isPlaying);
+        
+        Debug.Log($"Current tracks: {_currentTracks.Count}, Anchoring track: {(_AnchoringTrack ? _AnchoringTrack.name : "None")}");
+        foreach (AudioSource audioSourcetrack in Tracks)
+        {
+            if (!audioSourcetrack.isPlaying)
+            {
+                audioSourcetrack.loop = true;
+                audioSourcetrack.volume = 0f;
+            }
+        }
+    }
+
+    public void Fade(AudioSource track, AnimationCurve fadeCurve, SectionOfAnchoringTrack sectionOfAnchoringTrack, bool matchAnchoringTrackTime, bool fadingIn, float startOffsetSeconds, System.Action onComplete)
+    {
+        if (_trackFadingStates.ContainsKey(track) && _trackFadingStates[track])
+        {
+            Debug.LogWarning($"Track {track.name} is already fading. Ignoring new fade request.");
+            return;
+        }
+
+        var newFader = new TrackFader();
+        _trackFadingStates[track] = true; 
+        
+        StartCoroutine(newFader.Execute(_AnchoringTrack, track, fadeCurve, sectionOfAnchoringTrack, matchAnchoringTrackTime, fadingIn, startOffsetSeconds, () =>
+        {
+            _trackFadingStates[track] = false; 
+            
+            if (fadingIn && !_currentTracks.Contains(track))
+            {
+                _currentTracks.Add(track);
+                if (_AnchoringTrack == null)
+                {
+                    _AnchoringTrack = track;
+                    Debug.Log($"Set anchoring track to: {track.name}");
+                }
+            }
+
+            else if (!fadingIn && _currentTracks.Contains(track))
+            {
+                _currentTracks.Remove(track);
+                track.Stop(); 
+            }
+            
+            onComplete?.Invoke();
+            CurrentTrackCleanUp();
+        }));
+    }
+
+    public void FadeOutAllTracks(SectionOfAnchoringTrack section, AnimationCurve fadeOutCurve, float startOffsetSeconds, System.Action onComplete)
+    {
+        int tracksToFadeCount = _currentTracks.Count;
+        int fadeCompleteCount = 0;
+        
+        if (tracksToFadeCount == 0)
+        {
+            onComplete?.Invoke();
+            return;
+        }
+
+        List<AudioSource> tracksToFade = new List<AudioSource>(_currentTracks);
+        
+        foreach (AudioSource track in tracksToFade)
+        {
+            if (_trackFadingStates.ContainsKey(track) && _trackFadingStates[track])
+            {
+                tracksToFadeCount--;
+                continue;
+            }
+
+            var newFader = new TrackFader();
+            _trackFadingStates[track] = true;
+            
+            StartCoroutine(newFader.Execute(_AnchoringTrack, track, fadeOutCurve, section, false, false, startOffsetSeconds, () =>
+            {
+                _trackFadingStates[track] = false;
+                if (_currentTracks.Contains(track))
+                {
+                    _currentTracks.Remove(track);
+                    track.Stop();
+                }
+                
+                fadeCompleteCount++;
+                if (fadeCompleteCount >= tracksToFadeCount)
+                {
+                    onComplete?.Invoke();
+                    CurrentTrackCleanUp();
+                }
+            }));
+        }
+    }
+
+    public void ApplyTrackEffects(AudioSource sampleSource)
+    {
+        ResetAllPlayingTrackEffects();
+        
+        foreach (AudioSource track in _currentTracks)
+        {
+            var effectEditor = new AudioEffectEditor(track);
+            effectEditor.ApplyEffects(sampleSource);
+            _currentEffects.Add(effectEditor);
+        }
+    }
+
+    public void ResetAllPlayingTrackEffects()
+    {
+        foreach (AudioEffectEditor effect in _currentEffects)
+        {
+            effect.ResetEffects();
+        }
+        _currentEffects.Clear(); 
+    }
+
+    public void ResetEffects(AudioEffectEditor effect)
+    {
+        effect.ResetEffects();
+        _currentEffects.Remove(effect);
+    }
+
+    public void StopCurrentTracks()
+    {
+        foreach (AudioSource track in _currentTracks)
+        {
+            track.Stop();
+        }
+        _currentTracks.Clear();
+        _AnchoringTrack = null;
+    }
+    
+    public void PauseCurrentTracks()
+    {
+        foreach (AudioSource track in _currentTracks)
+        {
+            track.Pause();
+        }
+    }
+    
+    public void PlayCurrentTracks()
+    {
+        foreach (AudioSource track in _currentTracks)
+        {
+            track.Play();
+        }
+    }
+
+    public static MusicManager Instance { get; private set; }
+    
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+
+    private void OnDestroy()
+    {
+        if (Instance == this)
+        {
+            ResetAllPlayingTrackEffects();
+            Instance = null;
+        }
+    }
+}
---
+++ Assets/Code/scripts/General/Audio/MusicSystem/TrackFader.cs
@@ -0,0 +1,119 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using static IMusicTransition;
+
+[System.Serializable]
+public class TrackFader : IMusicTransition
+{
+    public IEnumerator Execute(AudioSource AnchoringTrack, AudioSource thisTrack, AnimationCurve fadeCurve, SectionOfAnchoringTrack sectionOfAnchoringTrack, bool MatchAnchoringTrackTime, bool FadeingIn, float StartOffsetSeconds, Action onComplete)
+    {
+        if ((AnchoringTrack == null || !AnchoringTrack.isPlaying || AnchoringTrack.clip == null) && FadeingIn && (MatchAnchoringTrackTime || sectionOfAnchoringTrack != SectionOfAnchoringTrack.OnTrigger))
+        {
+            Debug.LogError("No current clip is playing. Fading in while Matching Anchoring track time or timing it with a certain part of the track won't work.");
+            yield break;
+        }
+
+        // Anchor transition timing
+        if (sectionOfAnchoringTrack != SectionOfAnchoringTrack.OnTrigger)
+        {
+            if (sectionOfAnchoringTrack == SectionOfAnchoringTrack.TrackStart)
+            {
+                if (StartOffsetSeconds < 0)
+                {
+                    Debug.LogWarning("Anchor is track start but has a negative offset");
+                    yield break;
+                }
+                yield return new WaitUntil(() => AnchoringTrack.time >= StartOffsetSeconds);
+            }
+            else if (sectionOfAnchoringTrack == SectionOfAnchoringTrack.TrackMiddle)
+            {
+                yield return new WaitUntil(() => AnchoringTrack.time >= (AnchoringTrack.clip.length / 2) + StartOffsetSeconds);
+            }
+            else if (sectionOfAnchoringTrack == SectionOfAnchoringTrack.TrackEnd)
+            {
+                AnchoringTrack.loop = false;
+                Debug.Log($" {AnchoringTrack} - no longer looping");
+                if (StartOffsetSeconds <= 0)
+                {
+                    yield return new WaitUntil(() => AnchoringTrack.time >= AnchoringTrack.clip.length + StartOffsetSeconds);
+                }
+                else
+                {
+                    yield return new WaitUntil(() => !AnchoringTrack.isPlaying);
+                    yield return new WaitForSeconds(StartOffsetSeconds);
+                }
+            }
+        }
+        else // Anchor transition on trigger
+        {
+            if (StartOffsetSeconds < 0)
+            {
+                Debug.LogError("Cannot start on trigger with negative delay.");
+                yield break;
+            }
+            yield return new WaitForSeconds(StartOffsetSeconds);
+        }
+
+        if (FadeingIn)
+        {
+            yield return MusicManager.Instance.StartCoroutine(FadeIn(AnchoringTrack, thisTrack, fadeCurve, MatchAnchoringTrackTime));
+        }
+        else
+        {
+            yield return MusicManager.Instance.StartCoroutine(FadeOut(thisTrack, fadeCurve));
+        }
+        
+        onComplete?.Invoke();
+    }
+
+    private IEnumerator FadeIn(AudioSource AnchoringTrack, AudioSource thisTrack, AnimationCurve fadeInCurve, bool MatchAnchoringTrackTime)
+    {
+        float _fadeInDuration = fadeInCurve.keys[fadeInCurve.length - 1].time;
+
+        thisTrack.loop = true;
+        thisTrack.Play();
+        
+        if (MatchAnchoringTrackTime && AnchoringTrack != null && AnchoringTrack.isPlaying) 
+        {
+            thisTrack.time = AnchoringTrack.time;
+        }
+
+        float time = 0f;
+        while (time < _fadeInDuration)
+        {
+            time += Time.deltaTime;
+            
+            // Use the curve to get the volume multiplier (0-1), then multiply by original volume
+            float curveValue = fadeInCurve.Evaluate(time);
+            if (curveValue < 0f) curveValue = 0f;
+            else if (curveValue > 1f) curveValue = 1f;
+            thisTrack.volume = curveValue;
+            
+            Debug.Log($"Fade In {thisTrack} - Time: {time:F2}, Curve: {curveValue:F2}, Volume: {thisTrack.volume:F2}");
+            yield return null;
+        }        
+    }
+
+    private IEnumerator FadeOut(AudioSource thisTrack, AnimationCurve fadeOutCurve)
+    {
+        float _fadeOutDuration = fadeOutCurve.keys[fadeOutCurve.length - 1].time;
+
+        float _startVol = thisTrack.volume;
+
+        float time = 0f;
+        while (time < _fadeOutDuration)
+        {
+            time += Time.deltaTime;
+            
+            // Apply curve directly to the start volume
+            float curveValue = fadeOutCurve.Evaluate(time);
+            if (curveValue < 0f) curveValue = 0f;
+            else if (curveValue > 1f) curveValue = 1f;
+            thisTrack.volume = _startVol * curveValue;
+            
+            Debug.Log($"Fade Out {thisTrack}  - Time: {time:F2}, Curve: {curveValue:F2}, Volume: {thisTrack.volume:F2}");
+            yield return null;
+        }
+    }
+}
