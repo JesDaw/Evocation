39b85ed86524ac8920e3aba5d50cbf8fd7578e0c
AdamLenzini <adamclenzini@gmail.com>
2025-09-28 21:02:33 -0700

    Cutscene Update
    Tried to make pausing cutscenes automatic. Pausing is now broken. Will look into fixing soon

--- Assets/SceneBuilding/Scenes/WIP/TimelineController.cs
+++ Assets/SceneBuilding/Scenes/WIP/TimelineController.cs
@@ -1,25 +1,39 @@
 using UnityEngine;
 using UnityEngine.Playables;
 
+private bool isPaused = false;
+
 public class TimelineController : MonoBehaviour
 {
     PlayableDirector timeline;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         timeline = GetComponent<PlayableDirector>();
     }
 
-    void Update()
+    //void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.Z))
+    //        timeline.Play();
+    //    if (Input.GetKeyDown(KeyCode.X))
+    //        timeline.Pause();
+    //}
+
+    public void TogglePause()
     {
-        if (Input.GetKeyDown(KeyCode.Z))
-            timeline.Play();
-        if (Input.GetKeyDown(KeyCode.X))
-            timeline.Pause();
+        timeline.Resume();
+        isPaused = !isPaused;
     }
 
-    //public void PlayTimeline()
+    public void PauseAnimation()
+    {
+        timeline.Pause();
+        isPaused = !isPaused;
+    }
+    //private bool IsInputAllowed()
     //{
-    //    timeline.Play();
+    //    return timeline.state != PlayState.Playing;
     //}
 }
