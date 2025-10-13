4d01686eebc8767acd7581c43b446e4e92083994
Fish <thehuiq2006@gmail.com>
2025-10-12 18:52:21 -0700

    Spawn Condition stuff

--- Assets/Code/scripts/Levels/Gameplay/Objects/Characters/CpuUnits/Spawning/SpawningController.cs
+++ Assets/Code/scripts/Levels/Gameplay/Objects/Characters/CpuUnits/Spawning/SpawningController.cs
@@ -2,19 +2,37 @@ using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
 
-public abstract class SpawningController : MonoBehaviour
+public class SpawningController : MonoBehaviour
 {
     public SpawnObjects spawnObjects;
+    public float SpawnCondition;
+    private SpawningStatsInfo _info;
     public List<SpawningStatsInfo> SpawningInfo;
-    public abstract IEnumerator SpawnLoop();
+    void OnValidate()
+    {
+        SpawnCondition = Mathf.Clamp(SpawnCondition, 0f, 1f);
+    }
     private void Start()
     {
         StartCoroutine(SpawnLoopRepeater());
     }
-    public void Spawn(int stat)
+    public void Spawn(SpawningStatsInfo _stat)
     {
-        if (!SpawningInfo[stat].spawnable) return;
-        spawnObjects.SpawnFromSpawner(SpawningInfo[stat].stat);
+        if (_stat.ConditionLower > SpawnCondition) return;
+        if (_stat.ConditionUpper < SpawnCondition) return;
+        _info = _stat;
+        spawnObjects.SpawnFromSpawner(_stat.stat);
+    }
+
+    IEnumerator SpawnLoop()
+    {
+        foreach (SpawningStatsInfo _stat in SpawningInfo)
+        {
+            Spawn(_stat);
+            continue;
+        }
+
+        yield return new WaitForSeconds(_info.spawnRate);
     }
 
     private IEnumerator SpawnLoopRepeater()
@@ -26,9 +44,12 @@ public abstract class SpawningController : MonoBehaviour
     }
 }
 
+
 [System.Serializable]
 public class SpawningStatsInfo
 {
     public ScriptableStats stat;
-    public bool spawnable;
+    public float ConditionLower;
+    public float ConditionUpper;
+    public float spawnRate;
 }
