868d501f7c2a9fcac57dde8362462649323cdc39
JesDaw <jd281003@gmail.com>
2025-10-13 15:46:11 -0700

    Upgraded Diaologue system

---
+++ Assets/Code/scripts/Levels/Gameplay/UI/Dialogue/DailogueTrigger.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DailogueTrigger : MonoBehaviour
+{
+    public DialogueSO dialogueSO;
+    DialogueManager dialogueManager;
+    [SerializeField] internal UltEvents.UltEvent EndOfLines;
+
+    void Start()
+    {
+        dialogueManager = FindFirstObjectByType<DialogueManager>();
+    }
+    public void TriggerDailogue(InputAction.CallbackContext context)
+    {
+        if (!context.started || dialogueManager.DialogueActive) return;
+        Debug.Log("Trigger");
+        TriggerDailogue();
+    }
+    
+    public void TriggerDailogue()
+    {
+        dialogueManager.StartDialogue(dialogueSO, this);
+        dialogueManager.DialogueActive = true;
+    }
+    
+    public void EndDialogue()
+    {
+        dialogueManager.DialogueActive = false;
+        EndOfLines?.Invoke();
+    }
+}
