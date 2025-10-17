c56b03a13bae51ed8046502d168d1f2440bc0c18
JesDaw <jd281003@gmail.com>
2025-10-17 12:37:09 -0700

    finished merging dialogue with cutscenes

--- Assets/SceneBuilding/Scenes/WIP/Save System/PlayerData.cs
+++ Assets/SceneBuilding/Scenes/WIP/Save System/PlayerData.cs
@@ -9,14 +9,5 @@ public class PlayerData
     public int health;
     public float[] position;
 
-    public PlayerData (Player player)
-    {
-        level = player.level;
-        health = player.health;
 
-        position = new float[3];
-        position[0] = player.transform.position.x;
-        position[1] = player.transform.position.y;
-        position[2] = player.transform.position.z;
-    }
 }
--- Assets/SceneBuilding/Scenes/WIP/SaveSystem.cs
+++ Assets/SceneBuilding/Scenes/WIP/SaveSystem.cs
@@ -4,34 +4,5 @@ using System.Runtime.Serialization.Formatters.Binary;
 
 public static class SaveSystem
 {
-    public static void SavePlayer(Player player)
-    {
-        BinaryFormatter formatter = new BinaryFormatter();
-        string path = Application.persistentDataPath + "/player.txt";
-        Filestream stream = new Filestream(path, FileMode.Create);
 
-        PlayerData data = new PlayerData(player);
-
-        formatter.Serialize(stream, data);
-        stream.Close();
-    }
-
-    public static PlayerData LoadPlayer()
-    {
-        string path = Application.persistentDataPath + "/player.txt";
-        if (File.Exists(path))
-        {
-            BinaryFormatter formatter = new BinaryFormatter();
-            FileStream stream = new FileStream(path, FileMode.Open);
-
-            PlayerData data = formatter.Deserialize(stream);
-            stream.Close();
-
-            return data;
-        } else
-        {
-            Debug.LogError("Save file not found in " + path);
-            return null;
-        }
-    }
 }
