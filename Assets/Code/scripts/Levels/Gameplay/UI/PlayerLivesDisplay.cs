f25d6a5d8e074c8479b1066ca30a6e69e526301b
JesDaw <jd281003@gmail.com>
2025-10-26 23:52:45 -0700

    slight improvements on player life desplay

--- Assets/Code/scripts/Levels/Gameplay/UI/PlayerLivesDisplay.cs
+++ Assets/Code/scripts/Levels/Gameplay/UI/PlayerLivesDisplay.cs
@@ -3,36 +3,23 @@ using System.Collections.Generic;
 
 public class PlayerLivesDisplay : MonoBehaviour
 {
-    [SerializeField]
-    PlayerLivesManager livesManager;
-    [SerializeField]
-    List<GameObject> torchIcons = new List<GameObject>();
-    private int lastMaxLives = -1;
-
+    [SerializeField] PlayerLivesManager livesManager;
+    [SerializeField] List<GameObject> torchIcons = new List<GameObject>();
+    [SerializeField] List<GameObject> Fire = new List<GameObject>();
     void Start()
     {
         UpdateTorchDisplay();
     }
 
-    void Update()
-    {
-        if (livesManager != null && livesManager.MaxLives != lastMaxLives)
-        {
-            UpdateTorchDisplay();
-        }
-    }
-
-    void UpdateTorchDisplay()
+    public void UpdateTorchDisplay()
     {
         if (livesManager == null) return;
-
-        lastMaxLives = livesManager.MaxLives;
 
         for (int i = 0; i < torchIcons.Count; i++)
         {
             if (torchIcons[i] != null)
             {
-                torchIcons[i].SetActive(i < livesManager.MaxLives);
+                Fire[i].SetActive(i < livesManager.MaxLives);
             }
         }
     }
