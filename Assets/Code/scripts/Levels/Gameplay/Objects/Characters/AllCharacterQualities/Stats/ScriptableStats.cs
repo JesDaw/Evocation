985a9ad40101da20356b14b1cba4cc72deda8313
Fish <thehuiq2006@gmail.com>
2025-11-22 05:23:41 -0800

    Update ScriptableStats.cs

--- Assets/Code/scripts/Levels/Gameplay/Objects/Characters/AllCharacterQualities/Stats/ScriptableStats.cs
+++ Assets/Code/scripts/Levels/Gameplay/Objects/Characters/AllCharacterQualities/Stats/ScriptableStats.cs
@@ -6,6 +6,7 @@ using Evocation.Clans;
 public class ScriptableStats : ScriptableObject
 {
     [Header("Animation")]
+    public bool _Rotate;
     public AnimatorOverrideController _animator;
     public animationRigs[] _Sprites;
     public float _AnimationMoveSpeed = 1f;
