9ea84d5a02312ee251ff712052a7a67f3c971ed2
flowing sonata <barronsamachris@gmail.com>
2026-02-22 21:11:32 -0800

    moved around some animation events, sounds play on footstep
    Going to work on knockback soon, left a note in PlaeyerKnockedBackState.cs with a general idea of how it could be done

--- Assets/Code/scripts/Levels/Gameplay/Objects/Characters/CpuUnits/AnimationEventsController.cs
+++ Assets/Code/scripts/Levels/Gameplay/Objects/Characters/CpuUnits/AnimationEventsController.cs
@@ -13,4 +13,10 @@ public class AnimationEventsController : MonoBehaviour
         shouldAttack = false;
         return true;
     }
+    void OnStep()
+    {
+        FModAudioManager.instance.PlaySoundByName("attack");
+        // This could definitely be done in a much better way, it's not even modular and plays this single sound. ^^
+        // I would make it randomize the pitch, but I do not know where the "FModAudioManager" stores sounds.. - Chris S.
+    }
 }
