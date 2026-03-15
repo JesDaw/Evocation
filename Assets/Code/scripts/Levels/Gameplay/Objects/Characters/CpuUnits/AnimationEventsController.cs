6ea6982bd13c720f6f08d65de7f3615d1231528b
JesDaw <jd281003@gmail.com>
2026-03-15 13:14:41 -0700

    updated balancing room

--- Assets/Code/scripts/Levels/Gameplay/Objects/Characters/CpuUnits/AnimationEventsController.cs
+++ Assets/Code/scripts/Levels/Gameplay/Objects/Characters/CpuUnits/AnimationEventsController.cs
@@ -15,6 +15,11 @@ public class AnimationEventsController : MonoBehaviour
     }
     void OnStep()
     {
+        if (FModAudioManager.instance == null) 
+        {
+            //Debug.LogWarning("No audio manager");
+            return;
+        }
         FModAudioManager.instance.PlaySoundByName("attack");
         // This could definitely be done in a much better way, it's not even modular and plays this single sound. ^^
         // I would make it randomize the pitch, but I do not know where the "FModAudioManager" stores sounds.. - Chris S.
