f95ee682ab25913362b9fd158cdb047757dec5d6
JesDaw <jd281003@gmail.com>
2026-04-02 21:08:35 -0700

    made controls changing more robust but it still doesnt work

--- Assets/Code/scripts/General/InputSystem/GlobalInputManager.cs
+++ Assets/Code/scripts/General/InputSystem/GlobalInputManager.cs
@@ -204,6 +204,8 @@ public class GlobalInputManager : MonoBehaviour
 
     public void SetPauseMenuMode()
     {
+        Debug.Log($"SetPauseMenuMode called from:\n{System.Environment.StackTrace}");
+
         if(DebugLogs) Debug.Log("=========Input Mode: Pause Menu=========");
         DisableAllControls();
         EnableUIControls();
