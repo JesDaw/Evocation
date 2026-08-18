a9b3f9bd20517b938e1122175b71c01b8c9b3b68
JesDaw <jd281003@gmail.com>
2026-08-17 21:52:09 -0700

    mana bar actually works now

--- Assets/Code/Combat/PlayerApellAystem/UpdateMagicUIFunctions.cs
+++ Assets/Code/Combat/PlayerApellAystem/UpdateMagicUIFunctions.cs
@@ -1,12 +1,16 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-
+// make this singleton for easy referance
 public class UpdateMagicUIFunctions : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI totalMana;
     [SerializeField] TextMeshProUGUI cost;
     [SerializeField] Slider ManaBarImage;
+    void Start()
+    {
+        ManaBarImage.value = 0;
+    }
 
     public void UpdateSpellGUI(PlayerSpells _playerSpell)
     {
@@ -16,8 +20,6 @@ public class UpdateMagicUIFunctions : MonoBehaviour
     public void UpdateTotalMana(int _totalMana, int _changedMana)
     {
         totalMana.text = _totalMana.ToString();
-        ManaBarImage.value = _totalMana/100;
-        Debug.Log("here" + _totalMana);
-        
+        ManaBarImage.value = (float)_totalMana/100;        
     }
 }
